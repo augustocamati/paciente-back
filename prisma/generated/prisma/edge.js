4a2a87130a4dc1c0898d2c714b77738a959dc9d9
Augusto camati <augustocamati21@gmail.com>
2025-04-30 10:20:42 +0100

    gerando novo client do banco de dados

--- prisma/generated/prisma/edge.js
+++ prisma/generated/prisma/edge.js
@@ -217,12 +217,11 @@ const config = {
     "db"
   ],
   "activeProvider": "postgresql",
-  "postinstall": false,
   "inlineDatasources": {
     "db": {
       "url": {
         "fromEnvVar": "DATABASE_URL",
-        "value": null
+        "value": "postgresql://neondb_owner:npg_Lgj2pGZ6VzET@ep-patient-poetry-a4f55686-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require"
       }
     }
   },
